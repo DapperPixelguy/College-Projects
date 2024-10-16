9ff93863133f85abe3a6c1fc228b0dc00281b3c8
dapperpixelguy <gabrielrbtaylor@gmail.com>
2024-10-16 10:30:20 +0100

    Create

---
+++ story-generator/main.py
@@ -0,0 +1,73 @@
+import tkinter as tk
+from pyinflect import getInflection
+import pandas as pd
+
+chosen = 1
+
+
+def set_story(input):
+    global chosen
+    chosen = input
+    print(chosen)
+    fill_temp()
+
+
+root = tk.Tk()
+title = tk.Label(text='Story generator')
+root.geometry('200x200')
+entry_field = tk.Entry()
+confirm_button = tk.Button(text='Enter')
+rainy = tk.Button(text='Rainy Day', command=lambda: set_story(1))
+coffee = tk.Button(text='EX 2')
+
+templates = {
+    1: 'One [adjective] day, i was walking my [colour] pet [noun] when [plrnoun] started flying from the sky! I '
+       'called my friend [name] and she said one just landed right on her [place]! [adverb], there was raining ['
+       'plrnoun] as well and they were just going everywhere! My [noun] started [verb] and catching [plrnoun] with '
+       'his mouth to eat.'
+}
+
+def fill_temp():
+    template = templates[chosen]
+    adj = pd.DataFrame(pd.read_csv('WordnetAdjectives.csv'))
+    adv = pd.DataFrame(pd.read_csv('WordnetAdverbs.csv'))
+    nou = pd.DataFrame(pd.read_csv('WordnetNouns.csv'))
+    adj_count = template.count('[adjective]')
+    adv_count = template.count('[adverb]')
+    nou_count = template.count('[noun]')
+    plrnou_count = template.count('[plrnoun]')
+
+    for i in range(adj_count):
+        template = template.replace('[adjective]', adj['Word'].sample().values[0])
+
+    for i in range(adv_count):
+        template = template.replace('[adverb]', adv['Word'].sample().values[0])
+
+    for i in range(nou_count):
+        template = template.replace('[noun]', nou['Word'].sample().values[0])
+
+    for i in range(plrnou_count):
+        plrnoun = nou['Word'].sample().values[0]
+        inflection = getInflection(plrnoun, tag='NNS')
+
+        if inflection:
+            template = template.replace('[plrnoun]', inflection[0])
+
+        else:
+            template = template.replace('[plrnoun]', plrnoun + 's')
+
+    print(template)
+
+
+
+
+def rainy_day():
+ pass
+
+# Packing
+title.pack()
+entry_field.pack()
+confirm_button.pack()
+rainy.pack()
+root.mainloop()
+
