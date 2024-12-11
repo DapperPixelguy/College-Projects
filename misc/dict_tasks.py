a5842054b85e85f93487cb755a5539450cfa1253
DapperPixelguy <gabrielrbtaylor@gmail.com>
2024-12-11 10:30:31 +0000

    Dict tasks doesnt work

---
+++ misc/dict_tasks.py
@@ -0,0 +1,28 @@
+f = open('data/hitchhiker.txt', 'r')
+
+f = f.read()
+
+words = f.split(' ')
+word_freq_list = [{'word': "",
+                   'count': 0
+                   }]
+
+for word in words:
+    if word not in [word_freq_list[i]['word'] for i in range(len(word_freq_list))]:
+        word_freq_list.append({'word': word,
+                               'count': 1
+                               })
+    else:
+        for i in range(len(word_freq_list)):
+            if word_freq_list[i]['word'] == word:
+                word_freq_list[i]['count'] += 1
+
+results = [[word_freq_list[i]['word'], word_freq_list[i]['count']] for i in range(len(word_freq_list))]
+print(results)
+
+counts = [results[1] for result in results]
+
+print(max(counts))
+
+
+
