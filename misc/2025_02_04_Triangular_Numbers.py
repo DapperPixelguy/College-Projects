4caa35006f9d7b9fcc875bc79ff1dd3f43c1495e
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-02-04 13:21:14 +0000

    Updated visualisation

--- misc/2025_02_04_Triangular_Numbers.py
+++ misc/2025_02_04_Triangular_Numbers.py
@@ -1,12 +1,18 @@
-def tri(num=1, step=1):
-    if step < 100:
-        print(num)
-        tri(num+(step+1), step+1)
+def tri(num=1, step=1, display=False, max_iter=100):
+    if step < max_iter:
+        if display:
+            print(num)
+            print_tri(step)
+            print('-' * step*2)
+        else:
+            print(num)
+        tri(num+(step+1), step+1, display=display, max_iter=max_iter)
 
 def print_tri(step):
     for i in range(step):
+        i=i+1
         mid = int(step/2)
-        print(' '*(mid*2-i), end='')
+        print(' '*(mid*2-(i-1)), end='')
         print('# '*i)
 
-print_tri(10)
+tri(display=True, max_iter=10)
