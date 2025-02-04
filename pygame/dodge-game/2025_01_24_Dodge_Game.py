6c584d9a7ab17dd199a233c1a22a62de17d2841b
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-02-04 10:05:47 +0000

    Added death screen

--- pygame/dodge-game/2025_01_24_Dodge_Game.py
+++ pygame/dodge-game/2025_01_24_Dodge_Game.py
@@ -18,6 +18,8 @@ player_width, player_height = 50, 50
 player_x = WIDTH // 2
 player_y = HEIGHT - 2 * player_height
 player_speed = 7
+lives = 3
+immune = False
 
 obstacle_width, obstacle_height = 50, 50
 obstacle_x = random.randint(0, WIDTH-obstacle_width)
@@ -60,9 +62,9 @@ while running:
                             if event.key == pygame.K_ESCAPE:
                                 paused = False
 
-            if event.key == pygame.K_SPACE:
+            if event.key == pygame.K_SPACE or event.key == pygame.K_UP:
                 if len(bullets) < 2:
-                    bullets.append([(player_x+player_width/2)-bullet_width/2, player_y - 75, bullet_width, bullet_height])
+                    bullets.append([(player_x+player_width/2)-bullet_width/2, player_y - player_height - 10, bullet_width, bullet_height])
 
     screen.fill((0,0,0))
 
@@ -88,13 +90,13 @@ while running:
                     and bullet[0] + bullet_width > obstacle[0]
                     and bullet[1] < obstacle[1] + obstacle_height
                     and bullet[1] + bullet_height > obstacle[1]):
-                        obstacles.remove(obstacle)
+                        obstacle[1], obstacle[0] = random.randint(-obstacle_height-obstacle_height, -obstacle_height+obstacle_height), random.randint(0,WIDTH-obstacle_width)
                         bullets.remove(bullet)
                         score += 1
                         score_check(score)
 
         if obstacle[1] > HEIGHT:
-            obstacle[1] = -obstacle_height
+            obstacle[1] = random.randint(-obstacle_height-obstacle_height, -obstacle_height+obstacle_height)
             obstacle[0] = random.randint(0,WIDTH-obstacle_width)
             count += 1
             if count == ob_count:
@@ -102,20 +104,43 @@ while running:
                 count = 0
                 score_check(score)
 
+        if (player_x < obstacle[0] + obstacle_width
+            and player_x + player_width > obstacle[0]
+            and player_y < obstacle[1] + obstacle_height
+            and player_y + player_height > obstacle[1]):
 
-        if (
-                player_x < obstacle[0] + obstacle_width
-                and player_x + player_width > obstacle[0]
-                and player_y < obstacle[1] + obstacle_height
-                and player_y + player_height > obstacle[1]
-        ):
-            print(f"Game Over, your score was: {score}")
-            if int(score) > int(highscore):
-                with open('highscore.txt', 'w') as f:
-                    f.write(str(score))
-            running = False
+            if not immune:
+                lives -= 1
+                immune = True
+                threading.Timer(3, lambda: globals().__setitem__('immune', False)).start()
+
+            if lives <= 0:
+                x = None
+                print(f"Game Over, your score was: {score}")
+                if int(score) > int(highscore):
+                    with open('highscore.txt', 'w') as f:
+                        f.write(str(score))
+                threading.Timer(1, lambda: globals().__setitem__('x', 1)).start()
+                while True:
+                    screen.fill((0,0,0))
+                    if not x:
+                        pygame.draw.rect(screen, (200, 0, 255), (player_x, player_y, player_width, player_height))
+                    if x == 1:
+                        pygame.draw.rect(screen, (105,105,105), (player_x, player_y, player_width, player_height))
+                        threading.Timer(1, lambda: globals().__setitem__('x', 2)).start()
+                    elif x == 2:
+                        break
+
+                    clock.tick(60)
+                    pygame.display.flip()
+
+                running = False
+                pygame.quit()
+                sys.exit()
 
         pygame.draw.rect(screen, (255, 255, 255), (obstacle[0], obstacle[1], obstacle_width, obstacle_height))
+
+
 
     def remove_bullet(bullet):
         bullets.remove(bullet)
@@ -139,14 +164,18 @@ while running:
             bullet[1] += bullet_speed
 
 
-
-    pygame.draw.rect(screen, (200,0,255), (player_x, player_y, player_width, player_height))
+    if not immune:
+        pygame.draw.rect(screen, (200,0,255), (player_x, player_y, player_width, player_height))
+    if immune:
+        pygame.draw.rect(screen, (255, 0, 0), (player_x, player_y, player_width, player_height))
     with open ('highscore.txt', 'r') as f:
         highscore = f.read()
     score_text = font.render(f'Score: {score}       High Score: {highscore.strip()}', True, (0,0,255))
     ammo_text = font.render(f'Ammo', True, (0,0,255))
+    lives_text = font.render(f'Lives: {lives}', False, (0,0,255))
     screen.blit(ammo_text, (WIDTH-100, 10))
     screen.blit(score_text, (10,10))
+    screen.blit(lives_text, (10,50))
     pygame.display.flip()
     clock.tick(60)
 
