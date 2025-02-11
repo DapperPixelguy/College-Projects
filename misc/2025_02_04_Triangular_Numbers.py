bc06102760486534db1528f7cdf5552118540b00
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-02-11 09:45:34 +0000

    Triangular number thing

--- misc/2025_02_04_Triangular_Numbers.py
+++ misc/2025_02_04_Triangular_Numbers.py
@@ -1,3 +1,5 @@
+from itertools import count
+
 def tri(num=1, step=1, display=False, max_iter=100):
     if step < max_iter:
         if display:
@@ -9,8 +11,42 @@ def tri(num=1, step=1, display=False, max_iter=100):
         tri(num+(step+1), step+1, display=display, max_iter=max_iter)
 
 def print_tri(step):
+    triangle = []
     for i in range(1, step+1):
         print(' '*(step-i), end='')
         print('# '*i)
+        triangle.append((step-i, i))
 
-tri(display=True, max_iter=10)
+    return triangle
+
+tri(display=True)
+
+import pygame
+
+pygame.init()
+resolution = WIDTH, HEIGHT = 800,800
+screen = pygame.display.set_mode(resolution)
+size = 25
+
+def draw_tri(step):
+    triangle = []
+    for i in range(1, step+1):
+        # print(' '*(step-i), end='')
+        # print('# '*i)
+        triangle.append((step-i, i))
+        for j in range(1,i+1):
+            pygame.draw.rect(screen, (255,255,255), ((step-j)*size, i*size, size*i, size))
+    # print(triangle)
+
+running = True
+
+clock = pygame.time.Clock()
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            exit()
+
+    draw_tri(16)
+    pygame.display.flip()
+    clock.tick(60)
