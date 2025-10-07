47ed9fb13bc6a54e23bc4594c34cdc60d96f92f6
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-10-07 12:19:43 +0100

    Weather API app

---
+++ flask/2025_10_03_Weather/main.py
@@ -0,0 +1,54 @@
+from flask import Flask, request
+from flask import render_template
+import requests
+
+
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST','GET'])
+def index():
+    def get_weather(location='London'):
+        url = 'http://api.weatherapi.com/v1/current.json'
+        key = '76e28d4de7e044e99d6144910250310'
+        params = {
+            'key': key,
+            'q': location
+        }
+
+        response = requests.get(url, params=params)
+        if response.status_code == 200:
+            data = response.json()
+            return data['location']['name'], data['current']['condition']['text']
+        else:
+            fallback = {
+            'key': key,
+            'q': 'London'
+        }
+            response = requests.get(url, params=fallback)
+            data = response.json()
+            return data['location']['name'], data['current']['condition']['text']
+
+    def get_image(query='Sky'):
+        app_key = '_oXs4t9p9_i3Wf3e88XD6__7oExINVbRSBnCFe4B_2k'
+        url = 'https://api.unsplash.com/search/photos'
+        params = {
+            'client_id': app_key,
+            'query': query,
+            'page': 1,
+            'per_page': 1,
+        }
+        response = requests.get(url, params=params)
+        image = response.json()['results'][0]['urls']['small']
+        return image
+
+    if request.method == 'POST':
+        query = request.form.get('city').lower()
+
+        location, weather = get_weather(query)
+        return render_template('index.html', location=location, weather=weather, background=get_image(f'{weather} {location}'))
+    else:
+        location, weather = get_weather('London')
+        return render_template('index.html', location=location, weather=weather, background=get_image(f'{weather} {location}'))
+
+app.run(debug=True)
