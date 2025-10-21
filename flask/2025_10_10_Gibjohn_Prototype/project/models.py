ac4abb4579dbc77e9d4a0b2275821dd0e9f88224
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-10-21 11:39:15 +0100

    Added accessLevel column for admin assignment

--- flask/2025_10_10_Gibjohn_Prototype/project/models.py
+++ flask/2025_10_10_Gibjohn_Prototype/project/models.py
@@ -7,3 +7,12 @@ class User(UserMixin, db.Model):
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
     picture = db.Column(db.String(30))
+    accessLevel = db.Column(db.Integer, default=0)
+
+    @property
+    def access_name(self):
+        return {
+            0: '',
+            1: 'Admin',
+            2: 'Super Admin'
+        }.get(self.accessLevel, 'Unknown')
