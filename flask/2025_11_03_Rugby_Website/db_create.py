8df10729f0f8086bc62e48c0cb35ff33453f20bc
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-11-10 14:48:42 +0000

    Added dummy fixture data

--- flask/2025_11_03_Rugby_Website/db_create.py
+++ flask/2025_11_03_Rugby_Website/db_create.py
@@ -1,5 +1,19 @@
 from project import create_app, db
+from datetime import datetime
+from project.models import Fixture
+
+testFixtures = [{'team1': 'Norf fc', 'team2': 'souf fc', 'venue': 'down the pub', 'date': f'{datetime.now()}'},{'team1': 'Norf fc 2', 'team2': 'souf fc 2', 'venue': 'tobys', 'date': f'{datetime.now()}'}]
 
 with create_app().app_context():
     db.drop_all()
     db.create_all()
+    for fixture in testFixtures:
+        print(fixture)
+        data = Fixture()
+        data.team1 = fixture['team1']
+        data.team2 = fixture['team2']
+        data.date = fixture['date']
+        data.venue = fixture['venue']
+        print(data)
+        db.session.add(data)
+    db.session.commit()
