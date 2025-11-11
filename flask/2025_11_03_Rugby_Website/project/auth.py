413a3eaa361de87ca501ad01eeba8ce1cacff111
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-11-11 16:11:22 +0000

    Added signup and signup post routes, currently non-functional

--- flask/2025_11_03_Rugby_Website/project/auth.py
+++ flask/2025_11_03_Rugby_Website/project/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
+from werkzeug.security import generate_password_hash, check_password_hash
 
 auth = Blueprint('auth', __name__)
 
@@ -15,4 +16,16 @@ def login_post():
 
 @auth.route('/signup')
 def signup():
-    return 'Signup'
+    return render_template('signup.html')
+
+@auth.route('/signup', methods=['POST'])
+def signup_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    confirm_password = request.form.get('password-confirm')
+
+    print(email, password, confirm_password)
+
+    if password == confirm_password:
+        print(f'User with {email} successfully created')
+        return redirect(url_for('main.index'))
