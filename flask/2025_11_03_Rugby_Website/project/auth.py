b0ada29a70363bd2cc2986796e4b70ac0bb3d050
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-11-14 14:31:52 +0000

    Login and signup routes now function, added logout route

--- flask/2025_11_03_Rugby_Website/project/auth.py
+++ flask/2025_11_03_Rugby_Website/project/auth.py
@@ -1,31 +1,73 @@
+import flask
 from flask import Blueprint, render_template, redirect, url_for, flash, request
+from flask_login import login_user, logout_user, login_required, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from .models import User
+from . import db
+from .wrappers import logged_out_required
 
 auth = Blueprint('auth', __name__)
 
 @auth.route('/login')
+@logged_out_required
 def login():
     return render_template('login.html')
 
 @auth.route('/login', methods=['POST'])
+@logged_out_required
 def login_post():
     email = request.form.get('email')
     password = request.form.get('password')
-    print(email, password)
-    return redirect(url_for('auth.login'))
+
+    user = User.query.filter_by(email=email).first()
+
+    if not user:
+        flash({'text': 'User does not exist'}, category='error')
+        return redirect(url_for('auth.login'))
+
+    if check_password_hash(user.password, password):
+        login_user(user)
+        return redirect(url_for('main.index'))
+    else:
+        flash({'text': 'Incorrect email or password'}, category='error')
+        return redirect(url_for('auth.login'))
+
 
 @auth.route('/signup')
+@logged_out_required
 def signup():
     return render_template('signup.html')
 
 @auth.route('/signup', methods=['POST'])
+@logged_out_required
 def signup_post():
     email = request.form.get('email')
     password = request.form.get('password')
     confirm_password = request.form.get('password-confirm')
 
-    print(email, password, confirm_password)
+
+    if User.query.filter_by(email=email).first():
+        flash({'text': 'Email already registered'}, category='error')
+        return redirect(url_for('auth.signup'))
+
+    if password != confirm_password:
+        flash({'text': 'Passwords do not match'}, category='error')
+        return redirect(url_for('auth.signup'))
+
 
     if password == confirm_password:
-        print(f'User with {email} successfully created')
+        new_user = User()
+        new_user.password = generate_password_hash(password, salt_length=5)
+        new_user.email = email
+        db.session.add(new_user)
+        db.session.commit()
+
+        login_user(new_user, remember=True)
+
         return redirect(url_for('main.index'))
+
+@auth.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('main.index'))
