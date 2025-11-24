71a42548c58892b88c776d57bcbd474b3ca4fefa
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-11-24 11:46:57 +0000

    Now checks for login and accessLevel for edge cases, also returns a special json for request-only routes

--- flask/2025_11_03_Rugby_Website/project/wrappers.py
+++ flask/2025_11_03_Rugby_Website/project/wrappers.py
@@ -1,6 +1,6 @@
 from flask_login import current_user
 from functools import wraps
-from flask import redirect, url_for
+from flask import redirect, url_for, jsonify
 
 
 def logged_out_required(f):
@@ -11,12 +11,14 @@ def logged_out_required(f):
         return f(*args, **kwargs)
     return decorated_func
 
-def access_level_required(level):
+def access_level_required(level, requestonly=False):
     def decorator(f):
         @wraps(f)
         def decorated_func(*args, **kwargs):
-            if current_user.accessLevel >= level:
+            if current_user.is_authenticated and current_user.accessLevel >= level:
                 return f(*args, **kwargs)
+            if requestonly:
+                return jsonify({'Error': 'You are not authorized to access this page'}), 405
             return redirect(url_for('main.index'))
         return decorated_func
     return decorator
