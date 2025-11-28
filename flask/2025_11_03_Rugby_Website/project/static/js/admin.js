f9545b7de5c6f18efcf939d98b44333d0ce031b7
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-11-28 11:24:58 +0000

    Locks fixture input so 2 identical teams cant be selected

---
+++ flask/2025_11_03_Rugby_Website/project/static/js/admin.js
@@ -0,0 +1,27 @@
+let fixtureCreate = document.getElementById('fixture-create')
+let select = document.querySelectorAll('select')
+
+select.forEach((s)=>{
+    s.addEventListener('change', ()=>{
+        disableSelected()
+    })
+})
+
+function disableSelected(){
+
+    let arr = ['']
+
+    select.forEach(s=>{
+        s.querySelectorAll('option').forEach(o=>{
+            if (o.selected) arr.push(o.value)
+        })
+    })
+
+    select.forEach(s=>{
+        s.querySelectorAll('option').forEach(o=>{
+            o.disabled = !o.selected && arr.includes(o.value)
+        })
+    })
+    console.log(arr)
+
+}
