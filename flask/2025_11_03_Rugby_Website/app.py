9edb8badf00d99f695a61947f8998d025393a0f8
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-12-11 09:08:16 +0000

    Keep alive function for render

--- flask/2025_11_03_Rugby_Website/app.py
+++ flask/2025_11_03_Rugby_Website/app.py
@@ -1,6 +1,17 @@
 from project import create_app
+import schedule, threading, time
+from keepalive import ping
 
 app = create_app()
 
+def run_scheduler():
+    schedule.every(10).minutes.do(ping)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+
 if __name__ == '__main__':
     app.run(debug=True)
+
+    t = threading.Thread(target=run_scheduler, daemon=True)
+    t.start()
