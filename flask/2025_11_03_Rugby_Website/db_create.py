244ca3901a17a69a124c0d802fbd22f461771ba8
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-12-15 09:21:11 +0000

    Made into exportable function

--- flask/2025_11_03_Rugby_Website/db_create.py
+++ flask/2025_11_03_Rugby_Website/db_create.py
@@ -19,104 +19,106 @@ import re
 #     {'name': 'Oakridge Orcas', 'logo': '/static/logos/Brixham.png'}
 # ]
 
-teams = [
-]
+def db_create():
 
-for file in os.listdir('project/static/logos'):
-    if file.endswith('.png'):
-        team = {'name': '', 'logo': ''}
-        name = os.fsdecode(file).split('.')[0]
-        print(name)
-        name = re.findall('[A-Z][^A-Z]*', name)
-        name = ' '.join(name)
-        print(name)
-        logo = file
-
-        team['name'] = name
-        team['logo'] = f'static/logos/{logo}'
-
-        teams.append(team)
-
-# testFixtures = [
-#     {'team1': 'Meadowbrook RFC', 'team2': 'Trent Tigers', 'venue': 'Field House', 'date': '14/11/2025', 'time': '13:15'},
-#     {'team1': 'Westvale Wolves', 'team2': 'Barnfield RFC', 'venue': 'Community Ground', 'date': '14/11/2025', 'time': '15:00'},
-#     {'team1': 'Springfield Sappers', 'team2': 'Eastgate Eagles', 'venue': 'Another Example Venue', 'date': '14/11/2025', 'time': '12:00'},
-#     {'team1': 'Hilltop Harriers', 'team2': 'Northshore Knights', 'venue': 'Stadium Park', 'date': '14/11/2025', 'time': '18:30'},
-#     {'team1': 'Oakridge Orcas', 'team2': 'Riverside Rangers', 'venue': 'Example Venue', 'date': '14/11/2025', 'time': '12:00'}
-# ]
-
-leagues = ['Northern Premier League', 'Southern Championship Division', 'Central Elite Rugby League', 'Western Counties Shield League', 'Metropolitan Sports Premier Division']
-
-
-with create_app().app_context():
-    db.drop_all()
-    db.create_all()
-    for team in teams:
-        data = Team()
-        data.name= team['name']
-        data.logo = team['logo']
-        db.session.add(data)
-    db.session.commit()
-
-    for league in leagues:
-        x = League()
-        x.name = league
-        db.session.add(x)
-
-    db.session.commit()
-
-    teams_db = Team.query.all()
-    team_names = [t.name for t in teams_db]
-
-    venues = [
-        "Field House",
-        "Community Ground",
-        "Stadium Park",
-        "Example Venue",
-        "Another Example Venue"
+    teams = [
     ]
 
-    testFixtures = []
+    for file in os.listdir('project/static/logos'):
+        if file.endswith('.png'):
+            team = {'name': '', 'logo': ''}
+            name = os.fsdecode(file).split('.')[0]
+            print(name)
+            name = re.findall('[A-Z][^A-Z]*', name)
+            name = ' '.join(name)
+            print(name)
+            logo = file
 
-    for i in range(12):  # generate 5 fixtures
-        team1, team2 = random.sample(team_names, 2)  # ensures no duplicates
+            team['name'] = name
+            team['logo'] = f'static/logos/{logo}'
 
-        testFixtures.append({
-            'team1': team1,
-            'team2': team2,
-            'venue': random.choice(venues),
-            'date': '14/11/2025',  # or randomise later if you want
-            'time': '13:15'  # placeholder; can also randomise
-        })
+            teams.append(team)
 
-    for fixture in testFixtures:
-        print(datetime.strptime(fixture['date'], '%d/%m/%Y').date())
-        print(fixture)
-        data = Fixture()
-        data.team1 = Team.query.filter_by(name=fixture['team1']).first().id
-        data.team2 = Team.query.filter_by(name=fixture['team2']).first().id
-        data.date = datetime.strptime(fixture['date'], '%d/%m/%Y').date()
-        data.venue = fixture['venue']
-        data.time = datetime.strptime(fixture['time'], '%H:%M').time()
-        data.league = random.choice(League.query.all())
-        print(data)
-        db.session.add(data)
-    db.session.commit()
+    # testFixtures = [
+    #     {'team1': 'Meadowbrook RFC', 'team2': 'Trent Tigers', 'venue': 'Field House', 'date': '14/11/2025', 'time': '13:15'},
+    #     {'team1': 'Westvale Wolves', 'team2': 'Barnfield RFC', 'venue': 'Community Ground', 'date': '14/11/2025', 'time': '15:00'},
+    #     {'team1': 'Springfield Sappers', 'team2': 'Eastgate Eagles', 'venue': 'Another Example Venue', 'date': '14/11/2025', 'time': '12:00'},
+    #     {'team1': 'Hilltop Harriers', 'team2': 'Northshore Knights', 'venue': 'Stadium Park', 'date': '14/11/2025', 'time': '18:30'},
+    #     {'team1': 'Oakridge Orcas', 'team2': 'Riverside Rangers', 'venue': 'Example Venue', 'date': '14/11/2025', 'time': '12:00'}
+    # ]
 
-    # results = [{'team1_score': random.randint(0, 10), 'team2_score': random.randint(0, 10), 'fixture_id': x.id}
-    #            for x in Fixture.query.all()]
-    #
-    # for result in results:
-    #     data = Result()
-    #     data.team1_score = result['team1_score']
-    #     data.team2_score = result['team2_score']
-    #     data.fixture_id = result['fixture_id']
-    #     db.session.add(data)
-    # db.session.commit()
+    leagues = ['Northern Premier League', 'Southern Championship Division', 'Central Elite Rugby League', 'Western Counties Shield League', 'Metropolitan Sports Premier Division']
 
-    for fixture in Fixture.query.all():
-        table_entry_team1 = LeagueTable(team_id=fixture.team1, league_id=fixture.league.id)
-        db.session.add(table_entry_team1)
-        table_entry_team2 = LeagueTable(team_id=fixture.team2, league_id=fixture.league.id)
-        db.session.add(table_entry_team2)
-    db.session.commit()
+
+    with create_app().app_context():
+        db.drop_all()
+        db.create_all()
+        for team in teams:
+            data = Team()
+            data.name= team['name']
+            data.logo = team['logo']
+            db.session.add(data)
+        db.session.commit()
+
+        for league in leagues:
+            x = League()
+            x.name = league
+            db.session.add(x)
+
+        db.session.commit()
+
+        teams_db = Team.query.all()
+        team_names = [t.name for t in teams_db]
+
+        venues = [
+            "Field House",
+            "Community Ground",
+            "Stadium Park",
+            "Example Venue",
+            "Another Example Venue"
+        ]
+
+        testFixtures = []
+
+        for i in range(12):  # generate 5 fixtures
+            team1, team2 = random.sample(team_names, 2)  # ensures no duplicates
+
+            testFixtures.append({
+                'team1': team1,
+                'team2': team2,
+                'venue': random.choice(venues),
+                'date': '14/11/2025',  # or randomise later if you want
+                'time': '13:15'  # placeholder; can also randomise
+            })
+
+        for fixture in testFixtures:
+            print(datetime.strptime(fixture['date'], '%d/%m/%Y').date())
+            print(fixture)
+            data = Fixture()
+            data.team1 = Team.query.filter_by(name=fixture['team1']).first().id
+            data.team2 = Team.query.filter_by(name=fixture['team2']).first().id
+            data.date = datetime.strptime(fixture['date'], '%d/%m/%Y').date()
+            data.venue = fixture['venue']
+            data.time = datetime.strptime(fixture['time'], '%H:%M').time()
+            data.league = random.choice(League.query.all())
+            print(data)
+            db.session.add(data)
+        db.session.commit()
+
+        # results = [{'team1_score': random.randint(0, 10), 'team2_score': random.randint(0, 10), 'fixture_id': x.id}
+        #            for x in Fixture.query.all()]
+        #
+        # for result in results:
+        #     data = Result()
+        #     data.team1_score = result['team1_score']
+        #     data.team2_score = result['team2_score']
+        #     data.fixture_id = result['fixture_id']
+        #     db.session.add(data)
+        # db.session.commit()
+
+        for fixture in Fixture.query.all():
+            table_entry_team1 = LeagueTable(team_id=fixture.team1, league_id=fixture.league.id)
+            db.session.add(table_entry_team1)
+            table_entry_team2 = LeagueTable(team_id=fixture.team2, league_id=fixture.league.id)
+            db.session.add(table_entry_team2)
+        db.session.commit()
