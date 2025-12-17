5acdeb46e46b654a74d763ba7363df64aa9d3b31
DapperPixelguy <gabrielrbtaylor@gmail.com>
2025-12-17 12:35:32 +0000

    Removed some files

--- flask/2025_11_03_Rugby_Website/keepalive.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def ping():
-    try:
-        requests.get('https://college-projects-tslh.onrender.com')
-        print('Ping sent')
-    except Exception as e:
-        print(e)
